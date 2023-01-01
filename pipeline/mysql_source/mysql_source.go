558e21058d78f6307a145134d265444e939c54fb
vnvo <vahid.negahdari@gmail.com>
2023-01-01 03:29:22 +0200

    calculates start gtid set from last saved state

--- pipeline/mysql_source/mysql_source.go
+++ pipeline/mysql_source/mysql_source.go
@@ -2,6 +2,9 @@ package mysql_source
 
 import (
 	"context"
+	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/go-mysql-org/go-mysql/canal"
 	"github.com/go-mysql-org/go-mysql/mysql"
@@ -61,6 +64,7 @@ func (mys *MySQLBinlogSource) prepareCanal() error {
 func (mys *MySQLBinlogSource) Init(lastState string) error {
 	mys.fromGtid = nil
 	if len(lastState) > 0 {
+		//lastState = mys.getGTIDRange(masterGTID, lastGTID string)
 		gtidSet, err := mysql.ParseGTIDSet("mysql", lastState)
 		if err != nil {
 			log.Errorf("[%s] failed to parse last state: %v", mys.config.Name, err)
@@ -74,9 +78,24 @@ func (mys *MySQLBinlogSource) Init(lastState string) error {
 }
 
 func (mys *MySQLBinlogSource) Run(ctx context.Context) error {
+	masterGTID, err := mys.canal.GetMasterGTIDSet()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Debugf("master GTID=%s", masterGTID)
+
 	if mys.fromGtid != nil {
-		log.Info("starting binlog reader from GTID=%s", *mys.fromGtid)
-		mys.canal.StartFromGTID(*mys.fromGtid)
+		lastGTID := *mys.fromGtid
+		gtidStart := mys.getGTIDRange(masterGTID.String(), lastGTID.String())
+		startSet, err := mysql.ParseGTIDSet("mysql", gtidStart)
+		if err != nil {
+			log.Errorf("[%s] failed to parse calculated start state: %v", mys.config.Name, err)
+			return err
+		}
+
+		log.Infof("starting binlog reader from GTID=%s, calculated start=%s", lastGTID, gtidStart)
+		mys.canal.StartFromGTID(startSet)
 
 	} else {
 		log.Info("starting binlog reader from the begining (first GTID available)")
@@ -93,4 +112,37 @@ func (mys *MySQLBinlogSource) Close() {
 
 func (mys *MySQLBinlogSource) Query(query string) (*mysql.Result, error) {
 	return mys.canal.Execute(query)
+}
+
+func (mys *MySQLBinlogSource) getGTIDRange(masterGTID, lastGTID string) string {
+	// master=fa632d10-864b-11ed-843e-0242c0a86002:1-28
+	// lastgtid=fa632d10-864b-11ed-843e-0242c0a86002:18
+	log.Debugf("masterGTID=%s lastGTID=%s", masterGTID, lastGTID)
+	r := regexp.MustCompile("(.*):(.*)-(.*)")
+	master := r.FindStringSubmatch(masterGTID)[1:] //uuid, start, end
+	last := strings.Split(lastGTID, ":")           //uuid, end
+
+	if master[0] == last[0] {
+		if master[2] >= last[1] {
+			//master is ahead, safe to use the last
+			return fmt.Sprintf("%s:%s-%s", master[0], master[1], last[1])
+		} else {
+			// master is behind, this probably needs manual interventaion or
+			// some kind of pre-defined recovery strategy.
+			// for now, lets panic.
+			log.Error(
+				"[%s] last GTID is ahead of current master GTID\nMaster GTID=%s, Prensio GTID=%s",
+				mys.config.Name, masterGTID, lastGTID,
+			)
+			log.Errorf("Tip: Consider manually adjusting the pipeline state. Side effects: duplicate events!")
+			panic("")
+		}
+
+	} else {
+		log.Errorf(
+			"[%s] GTID mismatch. master=%s, prensio=%s",
+			mys.config.Name, masterGTID, lastGTID)
+		return lastGTID
+	}
+
 }
